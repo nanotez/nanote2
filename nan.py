fb04db58abb52329ac7e327a514714d54d5d7293
nanotez <97647355+nanotez@users.noreply.github.com>
2022-01-14 16:01:36 +0200

    Update and rename whoa.py to nan.py

---
+++ nan.py
@@ -0,0 +1,10 @@
+import requests
+
+nomer = input('Номер :')
+
+a= requests.post("https://rider.uklon.com.ua/api/v1/phone/send-code",
+                 json={"phone": nomer},)
+print(a.отправлено)
+except:
+			print('[-] Не отправлено!')
+    input
